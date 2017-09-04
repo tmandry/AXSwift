7c95b2ba591a02a298a8dc88907ca9d969d81358
Tyler Mandry <tmandry@gmail.com>
2017-09-04 17:48:59 -0500

    Add checkIsProcessTrusted

--- Sources/AXSwift.swift
+++ Sources/AXSwift.swift
@@ -1,4 +1,6 @@
-struct AXSwift {
-
-    var text = "Hello, World!"
+@discardableResult
+public func checkIsProcessTrusted(prompt: Bool = false) -> Bool {
+  let promptKey = kAXTrustedCheckOptionPrompt.takeUnretainedValue() as String
+  let opts      = [promptKey: prompt] as CFDictionary
+  return AXIsProcessTrustedWithOptions(opts)
 }
